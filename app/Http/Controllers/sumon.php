1686e82cd8dda67489994ed6409d60aa697df2a0
sshanto13 <sumancsediu@gmail.com>
2020-02-24 08:45:29 +0600

    1st commit

---
+++ app/Http/Controllers/sumon.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class sumon extends Controller
+{
+    public function index()
+    {
+        return view ('');
+    }
+}
